f9cf9afe3d53475c48d92e3d655c105bec9e27b8
Petrovick <petrovickg@hotmail.com>
2023-08-17 06:28:11 -0300

    fix: removing console.logs

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -45,9 +45,6 @@ export default class Product extends Entity implements ProductInterface  {
   }
 
   validate(): boolean {
-    console.log('this._name')
-    console.log(this._name);
-    console.log('this._name 2')
     if (this._id.length === 0) {
       this.notification.addError({
         context: 'product',
